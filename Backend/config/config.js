242ed7894424b065236f0e3714a71ca96e5f64ea
Vishwesh Anantha Krishnan <78962085+vishwesh-ak@users.noreply.github.com>
2023-11-10 15:15:02 +0530

    Update config.js

--- Backend/config/config.js
+++ Backend/config/config.js
@@ -1,16 +1,5 @@
-// config/config.js
-module.exports = {
-    database: {
-      url: 'http://vakcouch:couvakch@localhost:5984',
-      dbName: 'new_headcount',
-    },
-    rateLimit: {
-      windowMs: 1 * 60 * 1000, // 1 minute
-      max: 1, // limit each IP to 2 requests per windowMs
-      message: "Number of requests exceeded, try again later",
-    },
-    server: {
-      port: 5000,
-    },
-  };
-  
+DATABASE_URL=http://vakcouch:couvakch@localhost:5984
+DATABASE_NAME=final_headcount
+RATE_LIMIT_WINDOW_MS=60000
+RATE_LIMIT_MAX_REQUESTS=100
+SERVER_PORT=5000
